2da50cdc8f88b4aa52c80f1d93381d30a6ada4a3
DESKTOP-FF0S6N7\Rasel Rana <raselrana1147@gmail.com>
2022-02-25 23:12:22 +0600

    customer register

--- account/views.py
+++ account/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from account.forms import RegistrationForm
+
 
 # Create your views here.
+
+def register(request):
+    if request.user.is_authenticated:
+        return HttpResponse("You are authenticated")
+    else:
+        form = RegistrationForm
+        if request.method=="POST" or request.method=="post":
+            form=RegistrationForm(request.POST)
+            if form.is_valid():
+                form.save();
+                return HttpResponse("Your account has been created successfully")
+    context = {
+        'form': form
+    }
+    return render(request, 'register.html',context)
--- django_ecommerce/urls.py
+++ django_ecommerce/urls.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.conf import settings
 from django.contrib.staticfiles.urls import static,staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/',include('account.urls')),
 ]
 
 urlpatterns+= staticfiles_urlpatterns()
